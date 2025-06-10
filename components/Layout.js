2f4efeaf545a4348d8f42fcc37782381cf18517d
PreparedNurse <thejs962@yahoo.com>
2025-06-09 19:37:46 -0500

    Remove banner background animation

--- components/Layout.js
+++ components/Layout.js
@@ -27,7 +27,6 @@ const Header = styled.header`
     background-repeat: repeat-x;
     opacity: 0.8;
     z-index: 0;
-    animation: slideBackground 60s linear infinite;
   }
 
   &::after {
@@ -39,15 +38,6 @@ const Header = styled.header`
     bottom: 0;
     background: linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6));
     z-index: 0;
-  }
-
-  @keyframes slideBackground {
-    from {
-      transform: translateX(0);
-    }
-    to {
-      transform: translateX(-100%);
-    }
   }
 `;
 
